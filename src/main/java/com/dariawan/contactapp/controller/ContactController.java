eb9321f0bcba9e567d28cb37754a1f263d734805
teodesson <teodesson@gmail.com>
2019-10-28 17:36:31 +0800

    Contact Application in Spring Boot + JPA/Hibernate + PostgreSQL + SpringDoc OpenAPI 3

---
+++ src/main/java/com/dariawan/contactapp/controller/ContactController.java
@@ -0,0 +1,158 @@
+package com.dariawan.contactapp.controller;
+
+import com.dariawan.contactapp.domain.Address;
+import com.dariawan.contactapp.domain.Contact;
+import com.dariawan.contactapp.exception.BadResourceException;
+import com.dariawan.contactapp.exception.ResourceAlreadyExistsException;
+import com.dariawan.contactapp.exception.ResourceNotFoundException;
+import com.dariawan.contactapp.service.ContactService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Tag(name = "contact", description = "the Contact API")
+public class ContactController {
+    
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    private final int ROW_PER_PAGE = 5;
+    
+    @Autowired
+    private ContactService contactService;
+    
+    @Operation(summary = "Find Contacts by name", description = "Name search by %name% format", tags = { "contact" })
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "successful operation", 
+                content = @Content(array = @ArraySchema(schema = @Schema(implementation = Contact.class)))) })	
+    @GetMapping(value = "/contacts", produces = { "application/json", "application/xml" })
+    public ResponseEntity<List<Contact>> findAll(
+            @RequestParam(value="page", defaultValue="1") int pageNumber,
+            @RequestParam(required=false) String name) {
+        if (StringUtils.isEmpty(name)) {
+            return ResponseEntity.ok(contactService.findAll(pageNumber, ROW_PER_PAGE));
+        }
+        else {
+            return ResponseEntity.ok(contactService.findAllByName(name, pageNumber, ROW_PER_PAGE));
+        }
+    }
+
+    @Operation(summary = "Find contact by ID", description = "Returns a single contact", tags = { "contact" })
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "successful operation",
+                content = @Content(schema = @Schema(implementation = Contact.class))),
+        @ApiResponse(responseCode = "404", description = "Contact not found") })
+    @GetMapping(value = "/contacts/{contactId}", produces = { "application/json", "application/xml" })
+    public ResponseEntity<Contact> findContactById(@PathVariable long contactId) {
+        try {
+            Contact book = contactService.findById(contactId);
+            return ResponseEntity.ok(book);  // return 200, with json body
+        } catch (ResourceNotFoundException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // return 404, with null body
+        }
+    }
+    
+    @Operation(summary = "Add a new contact", description = "", tags = { "contact" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "201", description = "Contact created"), 
+        @ApiResponse(responseCode = "400", description = "Invalid input"), 
+        @ApiResponse(responseCode = "409", description = "Contact already exists") })	
+    @PostMapping(value = "/contacts", consumes = { "application/json", "application/xml" })
+    public ResponseEntity<Contact> addContact(@Valid @RequestBody Contact contact) 
+            throws URISyntaxException {
+        try {
+            Contact newContact = contactService.save(contact);
+            return ResponseEntity.created(new URI("/api/contacts/" + newContact.getId()))
+                    .body(contact);
+        } catch (ResourceAlreadyExistsException ex) {
+            // log exception first, then return Conflict (409)
+            logger.error(ex.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        } catch (BadResourceException ex) {
+            // log exception first, then return Bad Request (400)
+            logger.error(ex.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+    
+    @Operation(summary = "Update an existing contact", description = "", tags = { "contact" })
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "successful operation"),
+        @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
+        @ApiResponse(responseCode = "404", description = "Contact not found"),
+        @ApiResponse(responseCode = "405", description = "Validation exception") })
+    @PutMapping(value = "/contacts/{contactId}", consumes = { "application/json", "application/xml" })
+    public ResponseEntity<Contact> updateContact(@Valid @RequestBody Contact contact, 
+            @PathVariable long contactId) {
+        try {
+            contact.setId(contactId);
+            contactService.update(contact);
+            return ResponseEntity.ok().build();
+        } catch (ResourceNotFoundException ex) {
+            // log exception first, then return Not Found (404)
+            logger.error(ex.getMessage());
+            return ResponseEntity.notFound().build();
+        } catch (BadResourceException ex) {
+            // log exception first, then return Bad Request (400)
+            logger.error(ex.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+    
+    @Operation(summary = "Update an existing contact's address", description = "", tags = { "contact" })
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "successful operation"),
+        @ApiResponse(responseCode = "404", description = "Contact not found") })
+    @PatchMapping("/contacts/{contactId}")
+    public ResponseEntity<Void> updateAddress(@PathVariable long contactId,
+            @RequestBody Address address) {
+        try {
+            contactService.updateAddress(contactId, address);
+            return ResponseEntity.ok().build();
+        } catch (ResourceNotFoundException ex) {
+            // log exception first, then return Not Found (404)
+            logger.error(ex.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @Operation(summary = "Deletes a contact", description = "", tags = { "contact" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "successful operation"),
+        @ApiResponse(responseCode = "404", description = "Contact not found") })
+    @DeleteMapping(path="/contacts/{contactId}")
+    public ResponseEntity<Void> deleteContactById(@PathVariable long contactId) {
+        try {
+            contactService.deleteById(contactId);
+            return ResponseEntity.ok().build();
+        } catch (ResourceNotFoundException ex) {
+            logger.error(ex.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
